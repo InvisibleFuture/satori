b80c20794097f431e69b8e5ea03d4e676a91dca4
InvisibleFuture <huan0016@gmail.com>
2022-05-10 04:39:41 +0800

    debug res

--- server/middleware/session.js
+++ server/middleware/session.js
@@ -8,11 +8,11 @@ export default defineEventHandler(async event => {
         event.req.account = userinfo
       } else {
         console.log('会话仍有效, 但用户已不存在, 注销此会话')
-        setCookie(res, 'sid', '', {maxAge: -1})
+        setCookie(event.res, 'sid', '', {maxAge: -1})
       }
     } else {
       console.log('有cookie却已失效, 注销此 cookie(没有存储对应id)')
-      setCookie(res, 'sid', '', {maxAge: -1})
+      setCookie(event.res, 'sid', '', {maxAge: -1})
     }
   }
 })
