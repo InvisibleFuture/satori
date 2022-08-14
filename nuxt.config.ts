e742dd491aa8ae82d1c98962d52cf3c2801e1479
InvisibleFuture <huan0016@gmail.com>
2022-08-14 18:15:28 +0800

    public/img

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,6 @@ export default defineNuxtConfig({
     },
     title: 'satori',
     meta: [
-      { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1.0,user-scalable=no' }
     ],
   },
