e85adcac097b215740d8fe69903739b25f030665
桜華 <huan0016@gmail.com>
2022-11-24 07:57:52 +0800

    remove start

--- update.sh
+++ update.sh
@@ -14,12 +14,4 @@ scp -r .output.zip root@satori.love:~/docs/
 ssh root@satori.love "cd ~/satori; unzip -o -d ~/satori .output.zip; pm2 reload satori; rm .output.zip;"
 rm .output.zip
 
-## 在这之前, 应确保服务器上已经安装了对应版本的 nodejs
-## 以及使用 pm2 启动了名为 satori 的服务
-## 确保端口配置不会冲突
-# pm2 start node --name docs -- .output/server/index.mjs
-
-# build 时为服务更换默认端口
-# NITRO_PORT or PORT (defaults to 3000)
-# NITRO_HOST or HOST (defaults to '0.0.0.0')
-# node .output/server/index.mjs
+# pm2 start npm --name satori -- run preview
