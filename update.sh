1f52bebd1e89599d2249754378db7484b047b0df
桜華 <huan0016@gmail.com>
2022-11-24 09:56:53 +0800

    DEBUG update

--- update.sh
+++ update.sh
@@ -4,14 +4,13 @@
 cd $(dirname $0)
 npm run build
 
-# 打包上传以提高上传效率
+# 向服务器上传文件(打包上传以提高上传效率)
 zip -r .output.zip ./.output/*
-
-# 向服务器上传文件
-scp -r .output.zip root@satori.love:~/docs/
+scp -r .output.zip root@satori.love:~/satori/
+rm .output.zip
 
 # 解压文件
 ssh root@satori.love "cd ~/satori; unzip -o -d ~/satori .output.zip; pm2 reload satori; rm .output.zip;"
-rm .output.zip
+
 
 # pm2 start npm --name satori -- run preview
