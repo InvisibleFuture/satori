b3e896e1d33a8c1e03e6d9ec22ad3c70fa49072e
桜華 <huan0016@gmail.com>
2022-11-24 10:15:10 +0800

    tags

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,14 +1,34 @@
 <template lang="pug">
-.container.mx-auto.py-24.flex.flex-col
-  div(v-if="pending") Loading...
-  div.flex.flex-col.gap-6(v-else)
-    div(v-for="(item, index) in data.list", :key="item.id")
-      div.font-bold
-        NuxtLink(:to="`/blog/${item.id}`") {{ item.title }}
-      div
-        span.text-sm.text-gray-600 {{ item.createdAt }}
+.container.mx-auto.py-32.flex
+  main.flex-1
+    div(v-if="pending") Loading...
+    .flex.flex-col.gap-6(v-else)
+      div(v-for="(item, index) in data.list", :key="item.id")
+        .font-bold
+          NuxtLink(:to="`/blog/${item.id}`") {{ item.title }}
+        div
+          span.text-sm.text-gray-600 {{ item.createdAt }}
+  aside.w-64.py-2.flex.flex-col.gap-8(class="<sm:hidden")
+    .tags
+      span.font-bold # TAG
+      ul.flex.flex-wrap.gap-2.py-2
+        li.bg-gray-400.bg-opacity-10.px-2.rounded-md(
+          v-for="item in tags",
+          class="hover:text-pink-500"
+        ) {{ item.name }}
+    //div
+    //  span.font-bold # 归档
+    //  ul
+    //    li(v-for="index in 12") 2022 12 title
 </template>
 
 <script setup>
 const { data, pending } = useFetch("/api/blog");
+const tags = [
+  { name: "Ubuntu" },
+  { name: "Caddy" },
+  { name: "JavaScript" },
+  { name: "Stable Diffusion" },
+  { name: "Dreambooth" },
+];
 </script>
