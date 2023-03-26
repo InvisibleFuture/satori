63350860194685c3ac5bd7ae73d17382f6103b11
桜華 <huan0016@gmail.com>
2023-03-27 03:28:38 +0800

    验证发表权限

--- server/api/blog/index.js
+++ server/api/blog/index.js
@@ -14,25 +14,10 @@ const findTags = (item) => {
     return list
 }
 
-// 转换时间格式
-const rwdate = (utc) => {
-    let t = new Date(utc);
-    return t.getMonth() + 1 + "月 " + t.getDate() + ", " + t.getFullYear();
-}
-
 export default defineEventHandler(async event => {
 
+    const session = useStorage('session')
     const blog = useStorage('blog')
-
-    // 处理 POST 请求, 写入 markdown
-    if (event.req.method === 'POST') {
-        const { content } = await readBody(event)
-        const date = Date.now()
-        const data = { id: v4(), content, createdAt:date, updatedAt:date }
-        await blog.setItem(data.id, data)
-        data.html = marked(data.content, { breaks: true })
-        return data
-    }
 
     // 处理 GET 请求(读取 markdown 中的 md 文件列表)
     if (event.req.method === 'GET') {
@@ -53,6 +38,30 @@ export default defineEventHandler(async event => {
                 })
             }))
         }).then(list => list.sort((a, b) => b.updatedAt - a.updatedAt))
+    }
+
+    // 验证是否登录
+    const checkPermission = async () => {
+        const cookie = event.req.headers.cookie
+        const session_id = cookie ? cookie.split(';').find(item => item.trim().startsWith('session=')).split('=')[1] : null
+        const sessionData = session_id ? await session.getItem(session_id) : {}
+        return !!sessionData.uid
+    }
+
+    // 检查是否有权限操作
+    if (!await checkPermission()) {
+        event.res.statusCode = 401
+        return { success: false, message: '没有权限' }
+    }
+
+    // 处理 POST 请求, 写入 markdown
+    if (event.req.method === 'POST') {
+        const { content } = await readBody(event)
+        const date = Date.now()
+        const data = { id: v4(), content, createdAt:date, updatedAt:date }
+        await blog.setItem(data.id, data)
+        data.html = marked(data.content, { breaks: true })
+        return data
     }
 
     // 处理其他请求
