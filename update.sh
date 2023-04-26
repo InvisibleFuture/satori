f66974a4d1a55d7beb466417ed1a8774cf238002
桜華 <huan0016@gmail.com>
2023-04-27 02:36:20 +0800

    簡化結構, 轉移服務

--- update.sh
+++ update.sh
@@ -1,25 +1,26 @@
 #!/bin/sh
 
-# 编译静态资源(到相对目录)
+# 服務器地址
+SERVER="ubuntu@118.193.62.78"
+
+# 编译静态资源(到相对於腳本的目录)
 cd $(dirname $0)
 npm run build
 
 # 向服务器上传文件(打包上传以提高上传效率)
-zip -r .output.zip ./.output/*
-scp -r .output.zip root@satori.love:~/satori/
-rm .output.zip
+cd .output
+zip -9r satori.zip ./*
+scp -r satori.zip $SERVER:~/satori.zip
+cd ../
+rm -rf .output
 
-# 解压文件
-ssh root@satori.love "cd ~/satori; unzip -o -d ~/satori .output.zip; pm2 reload satori; rm .output.zip;"
-
-
-# pm2 start npm --name satori -- run preview
-
-# 同步数据
-# rsync -avz --delete --exclude-from=exclude.txt .
-
-# 上传文件
-# scp -r .
-
-# 重启服务
-# pm2 reload satori
+# 解压文件, 判斷 pm2 中是否存在 satori 進程, 如果存在則重啟, 否則啟動
+ssh $SERVER """
+    unzip -o -d ~/satori satori.zip;
+    cd ~/satori;
+    if pm2 list | grep satori; then
+        pm2 reload satori;
+    else
+        pm2 start node --name satori -- ./server/index.mjs;
+    fi
+    """
