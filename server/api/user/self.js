d8261f04c3c039be7ada80a642fd782ef0f8e1fa
觉 <huan0016@gmail.com>
2023-09-15 22:09:47 +0800

    DEBUG 修正cookie不存在时的错误

--- server/api/user/self.js
+++ server/api/user/self.js
@@ -4,14 +4,8 @@ export default defineEventHandler(async event => {
     const cookie = event.node.req.headers.cookie
 
     // 获取当前会话
-    const session_id = cookie ? cookie.split(';').find(item => item.trim().startsWith('session=')).split('=')[1] : null
+    const session_id = cookie ? cookie.split(';').find(item => item.trim().startsWith('session='))?.split('=')[1] : null
     const session_value = session_id ? await session.getItem(session_id) : null
-
-    //// 如果当前会话不存在, 则要求登录(401)
-    //if (!session_value) {
-    //    event.node.res.statusCode = 401
-    //    return { message: '请先登录' }
-    //}
 
     // 如果当前会话不存在, 则返回默认游客用户
     if (!session_value) {
@@ -26,13 +20,6 @@ export default defineEventHandler(async event => {
     // 获取当前用户
     const user_id = session_value ? session_value.user_id : null
     const user_value = user_id ? await user.getItem(user_id) : null
-
-    //// 如果当前用户不存在, 注销掉此会话, 要求登录(401)
-    //if (!user_value) {
-    //    await session.removeItem(session_id)
-    //    event.node.res.statusCode = 401
-    //    return { message: '请先登录' }
-    //}
 
     // 如果当前用户不存在, 则返回默认游客用户
     if (!user_value) {
