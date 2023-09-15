b50f734bc7172d0b0a22a6f920f57b3f236408d1
觉 <huan0016@gmail.com>
2023-09-15 22:48:25 +0800

    在登录页注册

--- pages/account/signin.vue
+++ pages/account/signin.vue
@@ -4,15 +4,16 @@ div.flex.justify-center.items-center.min-h-screen
     div.bg-white.shadow-md.rounded.px-8.pt-6.pb-8.mb-4
       form(v-on:submit.prevent="signin")
         div.mb-4
-          label.block.text-gray-700.text-sm.font-bold.mb-2(for="username") Username
+          label.block.text-gray-700.text-sm.font-bold.mb-2(for="username") username
           input#username.shadow.appearance-none.border.rounded.w-full.py-2.px-3.text-gray-700.leading-tight(
             class="focus:outline-none focus:shadow-outline",
             v-model="sign_data.name",
             type="text",
-            placeholder="Username"
+            placeholder="Username",
+            @change="usernameInputed"
           )
         div.mb-6
-          label.block.text-gray-700.text-sm.font-bold.mb-2(for="password") Password
+          label.block.text-gray-700.text-sm.font-bold.mb-2(for="password") password
           input#password.shadow.appearance-none.border.rounded.w-full.py-2.px-3.text-gray-700.mb-3.leading-tight(
             class="focus:outline-none focus:shadow-outline",
             v-model="sign_data.password",
@@ -23,28 +24,36 @@ div.flex.justify-center.items-center.min-h-screen
           button.bg-blue-500.text-white.font-bold.py-2.px-4.rounded(
             class="hover:bg-blue-700 focus:outline-none focus:shadow-outline"
             type="submit"
-          ) Sign In
-          a.inline-block.align-baseline.font-bold.text-sm.text-blue-500(
-            class="hover:text-blue-800"
-            href="#"
-          ) Forgot Password?
+          ) {{ sign_data.mode === 'signin' ? 'Sign In' : 'Sign Up' }}
 </template>
 
 <script setup>
 const router = useRouter()
 const account = useState("account")
-const sign_data = ref({ name: "", password: "" })
+const sign_data = ref({ name: "", password: "", mode: "signin" })
+
+const usernameInputed = () => {
+  const { name } = sign_data.value
+  if (name.length !== 0) {
+    $fetch(`/api/user`).then((data) => {
+      sign_data.value.mode = data.length ? "signin" : "signup"
+      console.log('mode:', sign_data.value.mode)
+    })
+  }
+}
 
 const signin = () => {
-  $fetch("/api/session", {
+  const { name, password, mode } = sign_data.value
+  $fetch(mode === "signin" ? "/api/session" : "/api/user", {
     method: "POST",
-    body: JSON.stringify(sign_data.value),
+    body: JSON.stringify({ name, password }),
     Headers: { "Content-Type": "application/json" }
   }).then((data) => {
     if (data.message) return alert(data.message)
     console.log('signin', data)
-    data.user.online = true
-    account.value = data.user
+    const user = data.user || data
+    user.online = true
+    account.value = user
     router.push("/account")
   })
 }
