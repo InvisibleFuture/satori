c233c0425b7f9dadc999cc3324639eab917bd0de
觉 <huan0016@gmail.com>
2023-09-24 18:22:00 +0800

    画廊异步

--- server/api/gallery/index.js
+++ server/api/gallery/index.js
@@ -20,13 +20,14 @@ export default defineEventHandler(async event => {
 
     // 处理 GET 请求(读取image中的图片文件列表)
     if (event.node.req.method === 'GET') {
-        const dirpath = fs.readdirSync(path.join(process.cwd(), 'data/gallery/image'))
-        const list = dirpath.map(file => {
+        const dirpath = await fs.promises.readdir(path.join(process.cwd(), 'data/gallery/image'))
+        const list = await Promise.all(dirpath.map(async file => {
             const file_path = path.join(process.cwd(), 'data/gallery/image', file)
-            const dimensions = imageSize(fs.readFileSync(file_path))
+            const dimensions = imageSize(await fs.promises.readFile(file_path))
             const image = '/api/gallery/' + file
             return { image, ...dimensions }
-        })
+        }))
+        console.log('list:', list)
         return { list }
     }
 })
