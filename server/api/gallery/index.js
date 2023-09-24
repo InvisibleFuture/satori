6487c1de59944cbfea288f841b25d40d1b13a654
觉 <huan0016@gmail.com>
2023-09-24 20:13:45 +0800

    减少重复检查

--- server/api/gallery/index.js
+++ server/api/gallery/index.js
@@ -2,22 +2,22 @@ import fs from 'fs'
 import path from 'path'
 import imageSize from 'image-size'
 
+// 如果数据目录不存在则创建
+if (!fs.existsSync(path.join(process.cwd(), 'data'))) {
+    fs.mkdirSync(path.join(process.cwd(), 'data'))
+}
+
+// 如果gallery目录不存在则创建
+if (!fs.existsSync(path.join(process.cwd(), 'data/gallery'))) {
+    fs.mkdirSync(path.join(process.cwd(), 'data/gallery'))
+}
+
+// 如果image目录不存在则创建
+if (!fs.existsSync(path.join(process.cwd(), 'data/gallery/image'))) {
+    fs.mkdirSync(path.join(process.cwd(), 'data/gallery/image'))
+}
+
 export default defineEventHandler(async event => {
-    // 如果数据目录不存在则创建
-    if (!fs.existsSync(path.join(process.cwd(), 'data'))) {
-        fs.mkdirSync(path.join(process.cwd(), 'data'))
-    }
-
-    // 如果gallery目录不存在则创建
-    if (!fs.existsSync(path.join(process.cwd(), 'data/gallery'))) {
-        fs.mkdirSync(path.join(process.cwd(), 'data/gallery'))
-    }
-
-    // 如果image目录不存在则创建
-    if (!fs.existsSync(path.join(process.cwd(), 'data/gallery/image'))) {
-        fs.mkdirSync(path.join(process.cwd(), 'data/gallery/image'))
-    }
-
     // 处理 GET 请求(读取image中的图片文件列表)
     if (event.node.req.method === 'GET') {
         const dirpath = await fs.promises.readdir(path.join(process.cwd(), 'data/gallery/image'))
